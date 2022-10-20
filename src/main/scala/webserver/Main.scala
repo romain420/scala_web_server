9fa0c1226fd12344e893d4f842867254530147d5
RomainD_ <romain.Dreuilhet@protonmail.com>
2022-10-20 13:10:47 +0200

    Tente de laisser le serveur ouvert meme apres la connection d'un client. Mais rien ne s'affiche pour le moment

--- src/main/scala/webserver/Main.scala
+++ src/main/scala/webserver/Main.scala
@@ -1,14 +1,16 @@
 package webserver
 
-import webserver.library.{Client, Server, WebResponse, WebRequest, Test}
+import webserver.library.{Client, Server, WebResponse, WebRequest, EchoServer, Test}
 
 @main def serverSide(): Unit =
   //println("Hello world!")
   //println(msg)
 
   //val myTest = Test
-  val myWebServer = Server(5000)
-  myWebServer.start
+//  val myWebServer = Server(5000)
+//  myWebServer.start
+  val myEchoServer = EchoServer(6000)
+  myEchoServer.start
   //println(myTest.hello)
   //println(myWebServer.get(WebRequest("test1")))
   //println(myWebServer.post(WebRequest("oui2")))
--- src/main/scala/webserver/library/EchoServer.scala
+++ src/main/scala/webserver/library/EchoServer.scala
@@ -1,36 +1,46 @@
 package webserver.library
 
-import java.net.{ServerSocket, Socket}
-import java.io.{BufferedReader, InputStreamReader, PrintWriter}
-import scala.util.Using
+import webserver.library.{WebRequest, WebResponse}
+import java.net.{ServerSocket}
+import java.io.{BufferedReader, PrintWriter, InputStreamReader}
+import scala.io.{Source}
+import scala.util.{Using}
 
-/*
-case class EchoServer(serverSocket: ServerSocket,
-                      clientSocket: Socket,
-                      out: PrintWriter,
-                      in: BufferedReader) {
+case class EchoServer(server: ServerSocket) {
+
+  def start: Unit = {
+    println("Waiting for a client...")
+    while(true) {
+      Using(server.accept()) { client =>
+        val out = new PrintWriter(client.getOutputStream, true)
+        val in = new BufferedReader(new InputStreamReader(client.getInputStream))
+        val greeting = in.readLine
+        if ("hello server" == greeting) {
+          println("Connection with a recognized client")
+          out.println("hello client")
+        }
+        else {
+          println("Connection with an unrecognized client")
+          out.println("unrecognised greeting")
+        }
+      }.fold( // fold acts here like an if
+        // if we have an error with the client...
+        error => println(s">>> client connection failure: ${error.getMessage}"),
+        // otherwise, we do nothing
+        _ => ()
+      )
+    }
+  }
+
+  def get(request: WebRequest): WebResponse = request.toWebResponse
+
+  def post(request: WebRequest): WebResponse = WebResponse("0001")
+
+  def put(request: WebRequest): WebResponse = WebResponse("0002")
+
+  def delete(request: WebRequest): WebResponse = WebResponse("0003")
 }
 
 object EchoServer {
-  def apply(port: Int): EchoServer = {
-
-    val serverSocket = ServerSocket(port)
-    val clientSocket = serverSocket.accept()
-    val out = PrintWriter(clientSocket.getOutputStream(), true)
-    val in = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-
-    val echoServer = EchoServer(
-      serverSocket = serverSocket,
-      clientSocket = clientSocket,
-      out = out,
-      in = in
-    )
-
-    echoServer
-  }
-
-  def applyForever(port: Int) = {
-    apply(port)
-    applyForever(port)
-  }
-}*/
+  def apply(port:Int): EchoServer = EchoServer(new ServerSocket(port))
+}
