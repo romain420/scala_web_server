a0a026345f43f0f326ab3bac8a2050c4b6e96a15
Théo <tgueuret@live.fr>
2022-10-22 21:37:27 +0200

    adding a processus for the received message from client, with a match-case. adding more 'Using', for out and in

--- src/main/scala/webserver/Main.scala
+++ src/main/scala/webserver/Main.scala
@@ -2,30 +2,23 @@ package webserver
 
 import webserver.library.{Client, Server, WebResponse, WebRequest, EchoServer, Test}
 
-@main def serverSide(): Unit =
-  //println("Hello world!")
-  //println(msg)
 
-  //val myTest = Test
-//  val myWebServer = Server(5000)
-//  myWebServer.start
-  val myEchoServer = EchoServer(6000)
+val server_port: Int = 8000
+
+@main def serverSide(): Unit =          // same as server but infinite loop for service
+  val myEchoServer = EchoServer(server_port)
   myEchoServer.start
-  //println(myTest.hello)
-  //println(myWebServer.get(WebRequest("test1")))
-  //println(myWebServer.post(WebRequest("oui2")))
-  //println(myWebServer.put(WebRequest("non3")))
-  //println(myWebServer.delete(WebRequest("bon4")))
-  //myWebServer.stop()
-//def msg = "I was compiled by Scala 3. :)"
 
+@main def serverTestSide(): Unit =      // one time use
+  val myServer = Server(server_port)
+  myServer.start
 
-@main def clientSide(): Unit =
-  val firstClient = Client("127.0.0.1", 5000, "hello server")
+@main def clientSide(): Unit =          // a client aht will be recognized by the server
+  val firstClient = Client("127.0.0.1", server_port, "hello server")
   firstClient.start
 
-@main def clientSideStranger(): Unit =
-  val secondClient = Client("127.0.0.1", 5000, "I send a wrong message")
+@main def clientSideStranger(): Unit =  // a client that will not be recognized by the server
+  val secondClient = Client("127.0.0.1", server_port, "I send a wrong message")
   secondClient.start
 
 
--- src/main/scala/webserver/library/EchoServer.scala
+++ src/main/scala/webserver/library/EchoServer.scala
@@ -1,7 +1,7 @@
 package webserver.library
 
 import webserver.library.{WebRequest, WebResponse}
-import java.net.{ServerSocket}
+import java.net.{ServerSocket, Socket}
 import java.io.{BufferedReader, PrintWriter, InputStreamReader}
 import scala.io.{Source}
 import scala.util.{Using}
@@ -12,24 +12,62 @@ case class EchoServer(server: ServerSocket) {
     println("Waiting for a client...")
     while(true) {
       Using(server.accept()) { client =>
-        val out = new PrintWriter(client.getOutputStream, true)
-        val in = new BufferedReader(new InputStreamReader(client.getInputStream))
-        val greeting = in.readLine
-        if ("hello server" == greeting) {
-          println("Connection with a recognized client")
-          out.println("hello client")
-        }
-        else {
-          println("Connection with an unrecognized client")
-          out.println("unrecognised greeting")
-        }
+        Using(new BufferedReader(new InputStreamReader(client.getInputStream))) { in =>
+          val message = in.readLine
+          processMessage(client, message)
+        }.fold(
+          error => {
+            println(s">>> cannot acquire the 'in', check the inputSteam: ${error.getMessage}")
+            closeServer()
+          },
+          _ => ()
+        )
       }.fold( // fold acts here like an if
         // if we have an error with the client...
-        error => println(s">>> client connection failure: ${error.getMessage}"),
+        error => {
+          println(s">>> client connection failure: ${error.getMessage}")
+          closeServer()
+        },
         // otherwise, we do nothing
         _ => ()
       )
     }
+  }
+
+  def processMessage(client: Socket, message: String): Unit = {
+    println(s"I received the following message:\n\t\"$message\"")
+    message match {
+      case "hello server" => {
+        sendMessage(client, s"Hello client, you are on port ${client.getPort()}")
+        sendMessage(client, s"We are now listening to you")
+        }
+      case "stop server"  => {
+        sendMessage(client, s"Copy client, we are closing the server")
+        closeServer()
+      }
+      case _ => {
+        sendMessage(client, s"I am going to send your message back")
+        sendMessage(client, message)
+      }
+    }
+    ()
+  }
+
+  def sendMessage(client: Socket, message: String): Unit = {
+    Using(new PrintWriter(client.getOutputStream, true)) { out =>
+      out.println(message)
+    }.fold(
+      error => {
+        println(s">>> could not send the message, check 'out': ${error.getMessage}")
+        closeServer()
+      },
+      _ => ()
+    )
+    ()
+  }
+
+  def closeServer(): Unit = {
+    ()
   }
 
   def get(request: WebRequest): WebResponse = request.toWebResponse
