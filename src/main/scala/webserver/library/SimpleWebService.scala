a610b2adadbef185247dc3cb8242d3f68b0d7313
Théo <tgueuret@live.fr>
2022-10-26 22:00:40 +0200

    add SimpleWebService

---
+++ src/main/scala/webserver/library/SimpleWebService.scala
@@ -0,0 +1,16 @@
+package webserver.library
+
+import webserver.library.{WebRequest, WebResponse}
+
+
+trait SimpleWebService() {
+
+  def get(request: WebRequest): WebResponse
+
+  def post(request: WebRequest): WebResponse
+
+  def put(request: WebRequest): WebResponse
+
+  def delete(request: WebRequest): WebResponse
+}
+
